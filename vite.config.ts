2a7759f658e0164bf13f73d19a76b5ff0c517b07
Eduardo da Silva <eduardo.silva@gmail.com>
2024-01-27 11:53:32 -0300

    icons just maskable not any on manifest

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           }
         ]
       },
