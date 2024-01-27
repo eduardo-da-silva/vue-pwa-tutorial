923349fdbfc5d7b896567574820150cc384937cf
Eduardo da Silva <eduardo.silva@gmail.com>
2024-01-27 11:50:41 -0300

    Adjust icon settings to builder

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,8 @@ export default defineConfig({
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any'
           },
           {
             src: 'pwa-512x512.png',
